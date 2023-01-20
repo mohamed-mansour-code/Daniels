5d7aebf1661d6c5bb6d5069176065194e0353602
Mmo768 <mohamedmansoura10@gmail.com>
2023-01-20 21:58:56 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,22 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:20,
+    nav: false,
+    dots:true,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        768:{
+            items:2,
+            nav:false
+        },
+        1200:{
+            items:3,
+            nav:true,
+            loop:false
+        }
+    }
+})
